473cd8052226761b7efa34a745529210655fc705
Niels V <nvdk@users.noreply.github.com>
2016-11-30 10:59:11 +0100

    return created object in map
    another coffeescript to js conversion mistake, forgot return statement in the inner function

--- addon/transforms/lang-string-set.js
+++ addon/transforms/lang-string-set.js
@@ -4,7 +4,7 @@ import Transform from 'ember-data/transform';
 export default Transform.extend({
   deserialize: function (serialized) {
     if (serialized && Ember.typeOf(serialized) === 'array') {
-      return serialized.map(function(o) { Ember.Object.create(o); });
+      return serialized.map(function(o) { return Ember.Object.create(o); });
     } else {
       throw "lang string set should be an array";
     }
