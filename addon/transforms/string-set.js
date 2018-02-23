c466f7c04639d698067f3fc433f06fde4e3ce82d
Erika Pauwels <erika.pauwels@gmail.com>
2018-02-23 12:42:20 +0100

    Export new transforms in app

--- addon/transforms/string-set.js
+++ addon/transforms/string-set.js
@@ -4,11 +4,11 @@ import Transform from 'ember-data/transform';
 
 const StringSet = Transform.extend({
   deserialize(serialized) {
-    assert(`Expected array but got ${typeOf(serialized)}`, (!serialized) || (typeOf(serialized) === "array"));
+    assert(`Expected array but got ${typeOf(serialized)}`, (!serialized) || (typeOf(serialized) === 'array'));
     return serialized || [];
   },
   serialize(deserialized) {
-    assert(`Expected array but got ${typeOf(deserialized)}`, (!deserialized) || (typeOf(deserialized) === "array"));
+    assert(`Expected array but got ${typeOf(deserialized)}`, (!deserialized) || (typeOf(deserialized) === 'array'));
     return deserialized || [];
   }});
 export default StringSet;
---
+++ app/transforms/language-string-set.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-mu-transform-helpers/transforms/language-string-set';
