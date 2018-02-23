4be6238f2656bcf6d7e8306b3c17d450ef1c34a3
Erika Pauwels <erika.pauwels@gmail.com>
2018-02-23 10:26:22 +0100

    Fix linting errors

--- addon/transforms/lang-string-set.js
+++ addon/transforms/lang-string-set.js
@@ -1,10 +1,11 @@
-import Ember from 'ember';
+import EmberObject from '@ember/object';
+import { typeOf } from '@ember/utils';
 import Transform from 'ember-data/transform';
 
 export default Transform.extend({
   deserialize: function (serialized) {
-    if (serialized && Ember.typeOf(serialized) === 'array') {
-      return serialized.map(function(o) { return Ember.Object.create(o); });
+    if (serialized && typeOf(serialized) === 'array') {
+      return serialized.map(function(o) { return EmberObject.create(o); });
     } else {
       throw "lang string set should be an array";
     }
