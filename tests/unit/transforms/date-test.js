fa973359e32c5fe2ae548863fc124fd168d61589
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-06-22 10:47:26 +0200

    Make sure the Date transform extends the Ember Data version
    We want to reuse the deserialize method of the Ember Data DateTransform class.

--- tests/unit/transforms/date-test.js
+++ tests/unit/transforms/date-test.js
@@ -4,9 +4,27 @@ import { setupTest } from 'ember-qunit';
 module('transform:date', 'Unit | Transform | date', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function (assert) {
-    let transform = this.owner.lookup('transform:date');
-    assert.ok(transform);
+  test('it deserializes date strings properly', function (assert) {
+    let transform = this.owner.lookup(
+      'transform:ember-mu-transform-helpers@date'
+    );
+
+    let deserialized = transform.deserialize('2021-12-21');
+
+    assert.ok(deserialized instanceof Date);
+    assert.equal(deserialized.getDate(), 21);
+    assert.equal(deserialized.getMonth(), 11);
+    assert.equal(deserialized.getFullYear(), 2021);
+  });
+
+  test('it serializes dates to ISO 8601 format without time information', function (assert) {
+    let transform = this.owner.lookup(
+      'transform:ember-mu-transform-helpers@date'
+    );
+
+    let date = new Date('2020-05-18');
+    let serialized = transform.serialize(date);
+
+    assert.equal(serialized, '2020-05-18');
   });
 });
